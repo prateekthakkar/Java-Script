c55cf1f97dd23f9b3fdeb25289dd805ff6a4fa0b
Prateek <Pratikthakkar790@gmail.com>
2020-03-05 02:13:32 +0530

    Js Day2

---
+++ js day2/script.js
@@ -0,0 +1,25 @@
+//Js Day 2 Task Define Varible And Varible Rules
+
+var firstname="Pratik"
+console.log(firstname);
+
+var lastname="Thakkar"
+var age=25;
+console.log(lastname);
+console.log(age);
+
+var fullage=true;
+console.log(fullage);
+
+var job;
+console.log(job);
+
+var job="Teacher";
+console.log(job);
+
+var _subject="javascript";
+console.log(_subject);
+
+var $subject="python";
+console.log($subject);
+
