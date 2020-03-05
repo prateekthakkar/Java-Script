02ed7d2ee455ccda0e3d821221553a5baf619a71
Prateek <Pratikthakkar790@gmail.com>
2020-03-06 01:33:37 +0530

    Varible Mutation And Type Coercion

---
+++ js day3/script.js
@@ -0,0 +1,15 @@
+var firstName="Pratik";
+var age=25;
+console.log(firstName +' '+ age); //Here Type Coercion Through Age Automatically Convert Into String 
+
+var job,isMarried; //Multiple Varible Declaration In Single Line.
+job = "Programer";
+isMarried = true;
+console.log(firstName + ' is a ' + age + ' years old ' + job +'.is he married? ' + isMarried);
+
+age="Twenty Four"; //Varible Mutation
+job="Developer";
+alert(firstName + ' is a ' + age + ' years old ' + job +'.is he married? ' + isMarried);
+
+var lastName = prompt("What is his lastName?"); //Prompt Box Show And Get Data From The User And Store Into lastName.
+console.log(firstName,lastName);
