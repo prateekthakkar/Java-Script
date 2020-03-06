7afdee025204b964b14f51a01e07dd7c565ae73f
Prateek <Pratikthakkar790@gmail.com>
2020-03-07 01:26:47 +0530

    Basic About Operators

---
+++ js day4/script.js
@@ -0,0 +1,52 @@
+//Basic About Operators.
+
+//var myage=2020-25;
+//var myfriendage=2020-30;
+//
+//console.log(myage);
+//console.log(myfriendage);
+//
+////OR
+//
+//var thisyear=2020;
+//var myage=thisyear - 25;
+//var myfriendage=thisyear - 28;
+//console.log(myage);
+//console.log(myfriendage);
+//
+////OR
+//
+//var thisyear,myage,myfriendage;
+//
+//thisyear=2020;
+//myage=thisyear - 25;
+//myfriendage=thisyear - 27;
+//console.log(myage);
+//console.log(myfriendage);
+//
+
+////Math Operators
+//
+//var thisyear=2020;
+//
+//console.log(thisyear+2);
+//console.log(thisyear-2);
+//console.log(thisyear*2);
+//console.log(thisyear/2);
+
+////Logical Operators
+//var me,myfriend;
+//me=25;
+//myfriend=28;
+//var older = me<myfriend;
+//console.log(older);
+
+//Type Of opener
+//var year,myage,newyear;
+//var year=2020;
+//newyear= myage > year;
+//console.log(newyear);
+//console.log(typeof year);
+//console.log(typeof myage);
+//console.log(typeof newyear);
+//console.log(typeof "Hello World!");
